860209bc3e12614f541d7a0308ad1d690e1c41ef
wd48 <kastrio.it@gmail.com>
2025-07-25 11:34:22 +0900

    add a SQLAlchemy (SQLite)

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,8 +1,27 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+'''
+2025-07-25
+- db 객체를 create_app 함수 안에서 생성하면 블루프린트와 같은 모듈에서 db 객체를 사용할 수 없으므로
+db, migrate 객체를 모듈 레벨에서 생성하고 (create_app 함수 밖), create_app 함수에서 초기화하는 방식으로 변경합니다.
+- 해당 객체 (db, migrate)를 앱에 등록할 때는 create_app 함수에서 init_app 함수를 통해 진행한다.
+'''
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
 
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # 블루프린트
     from .views import main_views
     app.register_blueprint(main_views.bp)
 
